993d3e4eb6af2f9fe078448c63664cff04b0d1ae
Luimarco Carrascal Diaz <lds.carrascal10@uniandes.edu.co>
2024-03-07 22:08:39 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import ProgressBar from './components/ProgressBar';
+import Navbar from './components/Navbar';
+import Timer from './components/Timer';
+
+
+
+function App() {
+  return (
+    <div>
+     
+      <Navbar/>
+      <ProgressBar/>
+      <Timer/>
+      
+  
+    
+      
+    </div>
+    
+  );
+}
+
+export default App;
